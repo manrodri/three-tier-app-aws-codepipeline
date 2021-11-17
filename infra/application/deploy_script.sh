970ddb146be08c861acb073ccde70c90ff881ed3
Manuel Rodriguez <lolo.edinburgh@gmail.com>
2021-11-17 15:16:23 +0000

    using shell script in build stage

--- infra/application/deploy_script.sh
+++ infra/application/deploy_script.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env sh
-echo "Running: terraform apply infra.tfplan"
+echo "Running ----> terraform apply infra.tfplan"
 terraform apply infra.tfplan
 retVal=$?
-if [ $retVal -ne 0 ]; then
-  echo "Terraform apply exit value === $retVal"
+if [ $? -e 0 ]; then
   exit 0
 else
-  echo "Terraform apply exit value === $retVal"
-  echo "Running: terraform destroy --auto-approve"
+  echo "Running ----> terraform destroy --auto-approve"
   terraform destroy --auto-approve
 fi
 
