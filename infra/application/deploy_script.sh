b364004dbba3fca2dde81dd79833c243fc77cc8a
Manuel Rodriguez <lolo.edinburgh@gmail.com>
2021-11-17 15:47:50 +0000

    using shell script in build stage

--- infra/application/deploy_script.sh
+++ infra/application/deploy_script.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env sh
 echo "Running ----> terraform apply infra.tfplan"
 terraform apply infra.tfplan
-retVal=$?
-if [ $? -e 0 ]; then
+if [ $? -eq 0 ]; then
   exit 0
 else
   echo "Running ----> terraform destroy --auto-approve"
--- infra/application/plan_script.sh
+++ infra/application/plan_script.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env sh
 
 terraform workspace new ${ENV}
-retVal=$?
-if [ $retVal -ne 0 ]; then
+if [ $? -ne 0 ]; then
     terraform workspace select ${ENV}
-    terraform plan -out infra.tfplan
-else
-  terraform plan -auto-approve
 fi
+  terraform validate
+  terraform plan -auto-approve
 
