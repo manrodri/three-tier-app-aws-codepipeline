69baa9fd8bf67957b3c8637bf23a172f41e31c88
Manuel Rodriguez <lolo.edinburgh@gmail.com>
2021-11-17 16:00:54 +0000

    using shell script in build stage

--- infra/application/deploy_script.sh
+++ infra/application/deploy_script.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env sh
+if $DELETE; then
+  echo "Running ----> terraform destroy --auto-approve"
+  terraform destroy --auto-approve
+  exit 0
+
 echo "Running ----> terraform apply infra.tfplan"
 terraform apply infra.tfplan
 if [ $? -eq 0 ]; then
